08771c8a773ffb0f278ec0196602363b9572da4d
Turi020 <turancan.dur@student.htldornbirn.at>
2025-01-07 08:23:32 +0100

    06_Zufahlszahlen fertig

---
+++ JS/06_Zufahlszahlen/script.js
@@ -0,0 +1,13 @@
+let randomNumber = Math.random() * 100;
+let random = Math.floor(randomNumber);
+
+console.log(random);
+
+//1 2 3 4 5 6 7 8 9 10 11 12 ... 19 20
+//Zufahlszahl zwischen 10 und 20
+
+randomNumber = Math.random() * 10 + 10;
+random = Math.floor(randomNumber);
+
+console.log(randomNumber);
+console.log(random);
