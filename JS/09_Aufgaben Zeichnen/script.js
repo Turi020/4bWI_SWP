33f897b19d45c964ade48378a3de5628097902fc
Turi020 <turancan.dur@student.htldornbirn.at>
2025-01-14 08:23:46 +0100

    Aufgabe 09_1 fertig

---
+++ JS/09_Aufgaben Zeichnen/script.js
@@ -0,0 +1,15 @@
+value = 36;
+
+for (let index = 0; index < value; index++) {
+    if (index % 5 == 0 && index % 7 == 0) {
+        console.log("FooBar");
+    } else if (index % 5 == 0) {
+        console.log("Bar");
+
+    } else if (index % 7 == 0) {
+        console.log("Foo");
+
+    } else { console.log(index); }
+
+
+}
