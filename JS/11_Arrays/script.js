f35a786733334692e2ca5c46f0adc7d84f91ea46
Turi020 <turancan.dur@student.htldornbirn.at>
2025-01-17 15:12:12 +0100

    11_Arrays fertig

---
+++ JS/11_Arrays/script.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 7;
+let c = 9;
+
+let arr = [5, 7, 9];
+console.log(arr[1]);
+
+arr.push(10);
+arr.push(3);
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+
+}
