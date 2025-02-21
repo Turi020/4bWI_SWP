7afc2637afe585225b584ca026f59247dd157be1
Turi020 <turancan.dur@student.htldornbirn.at>
2025-02-21 16:36:17 +0100

    01_TODO API (noch nicht fertig)

---
+++ JavaScript and Ajax/Todo mit RestAPI/script.js
@@ -0,0 +1,24 @@
+loadDataFromAPI;
+
+
+document.getElementById("btnLoad").addEventListener("click", function() {
+    loadDataFromAPI;
+});
+
+
+function loadDataFromAPI() {
+    fetch("https://67b89321699a8a7baef48e81.mockapi.io/todos")
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            let html = [];
+
+            json.forEach((todo) => {
+                html.push("<div><div>" + todo.title + "</div><img width='80px' src ='" + todo.image + "?id=" + Math.random() + "'/><div>----</div></div>");
+
+            });
+            document.getElementById("content").innerHTML = html.join("");
+
+
+        });
+}
