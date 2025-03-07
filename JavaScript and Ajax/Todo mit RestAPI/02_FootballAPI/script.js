74d19d6e62f8ae8d4234b83a69ccf80648fdfbb6
Turi020 <turancan.dur@student.htldornbirn.at>
2025-03-07 15:06:09 +0100

    02_FootballAPI noch nicht fertig

--- JavaScript and Ajax/Todo mit RestAPI/02_FootballAPI/script.js
+++ JavaScript and Ajax/Todo mit RestAPI/02_FootballAPI/script.js
@@ -1,10 +1,7 @@
-alert("Moin");
-
-
 loadFootballTable();
 
 function loadFootballTable() {
-    fetch("https://api.openligadb.de/getbltable/bl2/2024")
+    fetch("https://api.openligadb.de/getbltable/bl1/2024")
         .then((response) => response.json())
         .then((json) => {
             console.log(json);
@@ -20,14 +17,20 @@ function loadFootballTable() {
         });
 }
 document.getElementById("table").addEventListener("click", (event) => {
-    let selectedTeamId =
-        event.target.getAttribute("data-teamId");
+    let selectedTeamId = event.target.getAttribute("data-teamId");
+    console.log("selectedTeamId", selectedTeamId);
+    getNextMatchForTeam(selectedTeamId);
 })
 
 function getNextMatchForTeam(teamId) {
-    fetch("https://api.openligadb.de/getbltable/bl2/2024")
+    fetch(`https://api.openligadb.de/getnextmatchbyleagueteam/4741/${teamId}`)
         .then((response) => response.json())
         .then((json) => {
             console.log(json);
+            let html = "<div id = 'nextMatch'>";
+            html += `<div>${json.matchDateTime}</div>`;
+            html += `<div><img width = "40px" src ="${json.team1.teamIconUrl}"/>${json.team1.teamName} vs. ${json.team2.teamName}<img width = "40px" src ="${json.team2.teamIconUrl}"/></div>`;
+            html += "</div>";
+            document.getElementById("nextMatch").innerHTML = html;
         });
 }
